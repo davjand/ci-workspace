abd9fcdf9cd4d50fc87eb08087c438af25afbd32
davjand <dave@veodesign.co.uk>
2011-06-04 21:22:26 +0100

    Added batch file

---
+++ workspace/git_batch.bat
@@ -0,0 +1,45 @@
+git submodule add git://github.com/pointybeard/asdc.git extensions/asdc
+git submodule add git://github.com/nickdunn/dashboard.git extensions/dashboard
+git submodule add git://github.com/makenosound/dynamic_event_redirect.git extensions/dynamic_event_redirect
+git submodule add git://github.com/nickdunn/entity_diagram.git extensions/entity_diagram
+git submodule add git://github.com/czheng/datemodified.git extensions/datemodified
+git submodule add git://github.com/nickdunn/html_panel.git extensions/html_panel
+git submodule add git://github.com/klaftertief/imagecropper.git extensions/imagecropper
+git submodule add git://github.com/michael-e/uniqueuploadfield.git extensions/uniqueuploadfield
+git submodule add git://github.com/nickdunn/incrementnumberfield.git extensions/incrementnumberfield
+git submodule add git://github.com/symphonycms/numberfield.git extensions/numberfield
+
+git submodule add git://github.com/srowan-lewis/emailtemplatefilter.git extensions/emailtemplatefilter
+git submodule add git://github.com/rowan-lewis/globalresourceloader.git extensions/globalresourceloader
+git submodule add git://github.com/kanduvisla/importcsv.git extensions/importcsv
+
+git submodule add git://github.com/nickdunn/order_entries.git extensions/order_entries
+
+git submodule add git://github.com/nickdunn/publishfiltering.git extensions/publishfiltering
+git submodule add git://github.com/nickdunn/publish_tabs.git extensions/publish_tabs
+git submodule add git://github.com/knupska/quickds.git extensions/quickds
+git submodule add git://github.com/knupska/quickevent.git extensions/quickevent
+git submodule add git://github.com/nickdunn/search_index.git extensions/search_index
+git submodule add git://github.com/vlad-ghita/static_section.git extensions/static_section
+
+git submodule add git://github.com/nilshoerrmann/subsectionmanager.git extensions/subsectionmanager
+
+git submodule add git://github.com/nickdunn/symql.git extensions/symql
+git submodule add git://github.com/kanduvisla/symphony-ckeditor.git extensions/ckeditor
+
+git submodule add git://github.com/makenosound/jquery_date_picker.git extensions/jquery_date_picker
+git submodule add git://github.com/eKoeS/better_forms.git extensions/better_forms
+git submodule add git://github.com/creativedutchmen/email_template_manager.git extensions/email_template_manager
+git submodule add git://github.com/kanduvisla/involve_your_client.git extensions/involve_your_client
+git submodule add git://github.com/czheng/documenter.git extensions/documenter
+git submodule add git://github.com/bauhouse/configuration.git extensions/configuration
+git submodule add git://github.com/ahwayakchih/sortdatasourcebyparam.git extensions/sortdatasourcebyparam
+git submodule add git://github.com/eKoeS/edui.git extensions/edui
+git submodule add git://github.com/nickdunn/db_sync.git extensions/db_sync
+git submodule add git://github.com/nickdunn/sitemap.git extensions/sitemap
+git submodule add git://github.com/nils-werner/dump_db.git extensions/dump_db
+
+
+
+
+
