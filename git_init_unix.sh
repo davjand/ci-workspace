7f3fc90bb9e237d65cbc2beb1ac3afd980e35c00
David Anderson <dave@veodesign.co.uk>
2012-07-21 16:10:15 +0100

    Symphony extensions added to prevent some timeout errors on certain connections

--- git_init_unix.sh
+++ git_init_unix.sh
@@ -6,43 +6,78 @@
 #
 ###
 
-# INITIAL - UNCOMMENT TO RUN AS A COMPLETE INSTALLER
+### INITIAL - UNCOMMENT TO RUN AS A COMPLETE INSTALLER
+#
 
 #git init
 #git remote add workspace git@github.com:davjand/sym-workspace.git
 #git fetch workspace
 #git merge workspace/master --no-edit
 
+#
 ###
 
 
-# Pull Symphony
+### Pull Symphony
+#
+
 git remote add symphony git@github.com:symphonycms/symphony-2.git
 git fetch symphony
 git merge symphony/master --no-edit
 
+#
 ###
 
-# Sort out the git ignore from symphony
+
+
+### Re-add all symphony default extensions as can prevent errors
+#
+rm .gitmodules
+git submodule add git@github.com:symphonycms/debugdevkit.git extensions/debugdevkit --recursive
+git submodule add git@github.com:symphonycms/export_ensemble.git extensions/export_ensemble --recursive
+git submodule add git@github.com:symphonycms/jit_image_manipulation.git extensions/jit_image_manipulation --recursive
+git submodule add git@github.com:symphonycms/maintenance_mode.git extensions/maintenance_mode --recursive
+git submodule add git@github.com:symphonycms/markdown.git extensions/markdown --recursive
+git submodule add git@github.com:symphonycms/profiledevkit.git extensions/profiledevkit --recursive
+git submodule add git@github.com:symphonycms/selectbox_link_field.git extensions/selectbox_link_field --recursive
+git submodule add git@github.com:symphonycms/xssfilter.git extensions/xssfilter --recursive
+#
+###
+
+
+### Sort out the git ignore from symphony
+#
+
 rm .gitignore
 mv ./workspace.gitignore ./.gitignore
 
+#
 ###
 
-#Grab extensions
+###Grab extensions
+#
+
 sh git_submodules.sh
 
+#
 ###
 
-# Init submodules
+### Init submodules
+#
+
 git submodule update --init
 
+#
 ###
 
-# REMOTE REPO SETUP
+### REMOTE REPO SETUP
+#
 
 git add -A
 git commit -m "Initial Commit"
 
 #git remote add origin git@github.com:davjand/ {INSERT REPO} .git
 #git push origin master
+
+#
+###
