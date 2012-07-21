641f98cd94445aa72266b1f0d860978327a9ac56
David Anderson <dave@veodesign.co.uk>
2012-07-21 15:28:48 +0100

    Update git urls in submodules file

--- git_submodules.sh
+++ git_submodules.sh
@@ -10,16 +10,16 @@
 ### EXTENSIONS THAT PROVIDE SPECIFIC FUNCTIONALITY
 #
 
-###git submodule add git://github.com/kanduvisla/author_roles.git extensions/author_roles --recursive
-###git submodule add git://github.com/symphonycms/members.git extensions/members --recursive
-###git submodule add git://github.com/scottkf/massuploadutility.git extensions/massuploadutility --recursive
-###git submodule add git://github.com/lewiswharf/ab_split_test.git extensions/ab_split_test --recursive
-###git submodule add git://github.com/klaftertief/imagecropper.git extensions/imagecropper --recursive
+###git submodule add git@github.com:kanduvisla/author_roles.git extensions/author_roles --recursive
+###git submodule add git@github.com:symphonycms/members.git extensions/members --recursive
+###git submodule add git@github.com:scottkf/massuploadutility.git extensions/massuploadutility --recursive
+###git submodule add git@github.com:lewiswharf/ab_split_test.git extensions/ab_split_test --recursive
+###git submodule add git@github.com:klaftertief/imagecropper.git extensions/imagecropper --recursive
+###git submodule add git@github.com:pixelninja/sitemap_xml.git extensions/sitemap_xml --recursive
 
-git submodule add git://github.com/pixelninja/sitemap_xml.git extensions/sitemap_xml --recursive
-git submodule add git://github.com/symphonists/xmlimporter.git extensions/xmlimporter --recursive
-git submodule add git://github.com/creativedutchmen/email_template_manager.git extensions/email_template_manager --recursive
-git submodule add git://github.com/kanduvisla/importcsv.git extensions/importcsv --recursive
+git submodule add git@github.com:symphonists/xmlimporter.git extensions/xmlimporter --recursive
+git submodule add git@github.com:creativedutchmen/email_template_manager.git extensions/email_template_manager --recursive
+git submodule add git@github.com:kanduvisla/importcsv.git extensions/importcsv --recursive
 
 #
 ###
@@ -27,82 +27,82 @@ git submodule add git://github.com/kanduvisla/importcsv.git extensions/importcsv
 
 ### ADMINISTRATION EXTENSIONS
 #
-git submodule add git://github.com/symphonists/tracker.git extensions/tracker --recursive
-git submodule add git://github.com/Solutions-Nitriques/duplicate_section.git extensions/duplicate_section --recursive
-git submodule add git://github.com/nickdunn/publishfiltering.git extensions/publishfiltering --recursive
-git submodule add git://github.com/alpacaaa/resave_entries.git extensions/resave_entries --recursive
-git submodule add git://github.com/nickdunn/sitemap.git extensions/sitemap --recursive
-git submodule add git://github.com/nickdunn/entity_diagram.git extensions/entity_diagram --recursive
-git submodule add git://github.com/Solutions-Nitriques/anti_brute_force.git extensions/anti_brute_force --recursive
+git submodule add git@github.com:symphonists/tracker.git extensions/tracker --recursive
+git submodule add git@github.com:Solutions-Nitriques/duplicate_section.git extensions/duplicate_section --recursive
+git submodule add git@github.com:nickdunn/publishfiltering.git extensions/publishfiltering --recursive
+git submodule add git@github.com:alpacaaa/resave_entries.git extensions/resave_entries --recursive
+git submodule add git@github.com:nickdunn/sitemap.git extensions/sitemap --recursive
+git submodule add git@github.com:nickdunn/entity_diagram.git extensions/entity_diagram --recursive
+git submodule add git@github.com:Solutions-Nitriques/anti_brute_force.git extensions/anti_brute_force --recursive
 #
 ###
 
 
 ### USER INTERFACE
 #
-git submodule add git://github.com/6ui11em/form_select2.git extensions/form_select2 --recursive
-git submodule add git://github.com/vlad-ghita/pretty_photo.git extensions/pretty_photo --recursive
-git submodule add git://github.com/Solutions-Nitriques/save_and_return.git extensions/save_and_return --recursive
+git submodule add git@github.com:6ui11em/form_select2.git extensions/form_select2 --recursive
+git submodule add git@github.com:vlad-ghita/pretty_photo.git extensions/pretty_photo --recursive
+git submodule add git@github.com:Solutions-Nitriques/save_and_return.git extensions/save_and_return --recursive
 #
 ###
 
 
 ### DATABASE EXTENSIONS
 #
-git submodule add git://github.com/nils-werner/dump_db.git extensions/dump_db --recursive
+git submodule add git@github.com:nils-werner/dump_db.git extensions/dump_db --recursive
 #
 ###
 
 
 ### DEVELOPMENTAL WORKFLOW
 #
-git submodule add git://github.com/nils-werner/less_compiler.git extensions/less_compiler --recursive
-git submodule add git://github.com/nils-werner/xslresourceloader.git extensions/xslresourceloader --recursive
-git submodule add git://github.com/ahwayakchih/sortdatasourcebyparam.git extensions/sortdatasourcebyparam --recursive
-git submodule add git://github.com/nickdunn/symql.git extensions/symql --recursive
+git submodule add git@github.com:nils-werner/less_compiler.git extensions/less_compiler --recursive
+git submodule add git@github.com:nils-werner/xslresourceloader.git extensions/xslresourceloader --recursive
+git submodule add git@github.com:ahwayakchih/sortdatasourcebyparam.git extensions/sortdatasourcebyparam --recursive
+git submodule add git@github.com:nickdunn/symql.git extensions/symql --recursive
 #
 ###
 
 
 ### EVENTS
 #
-git submodule add git://github.com/brendo/default_event_values.git extensions/default_event_values --recursive
+git submodule add git@github.com:brendo/default_event_values.git extensions/default_event_values --recursive
 #
 ###
 
 
 ### FIELDS
 #
-git submodule add git://github.com/nickdunn/publish_tabs.git extensions/publish_tabs --recursive
-git submodule add git://github.com/rowan-lewis/textboxfield.git extensions/textboxfield --recursive
-git submodule add git://github.com/michael-e/uniqueuploadfield.git extensions/uniqueuploadfield --recursive
-git submodule add git://github.com/symphonycms/numberfield.git extensions/numberfield --recursive
-git submodule add git://github.com/hananils/subsectionmanager.git extensions/subsectionmanager --recursive
-git submodule add git://github.com/hananils/datetime.git extensions/datetime --recursive
-git submodule add git://github.com/nickdunn/order_entries.git extensions/order_entries --recursive
-git submodule add git://github.com/kanduvisla/tabbed_textarea.git extensions/tabbed_textarea --recursive
-git submodule add git://github.com/symphonists/referencelink.git extensions/referencelink --recursive
+git submodule add git@github.com:nickdunn/publish_tabs.git extensions/publish_tabs --recursive
+git submodule add git@github.com:rowan-lewis/textboxfield.git extensions/textboxfield --recursive
+git submodule add git@github.com:michael-e/uniqueuploadfield.git extensions/uniqueuploadfield --recursive
+git submodule add git@github.com:symphonycms/numberfield.git extensions/numberfield --recursive
+git submodule add git@github.com:hananils/subsectionmanager.git extensions/subsectionmanager --recursive
+git submodule add git@github.com:hananils/datetime.git extensions/datetime --recursive
+git submodule add git@github.com:nickdunn/order_entries.git extensions/order_entries --recursive
+git submodule add git@github.com:kanduvisla/tabbed_textarea.git extensions/tabbed_textarea --recursive
+git submodule add git@github.com:symphonists/referencelink.git extensions/referencelink --recursive
 #
 ###
 
 
 ### TEXT FORMATTERS
 #
-git submodule add git://github.com/kanduvisla/ckeditor.git extensions/ckeditor --recursive
+git submodule add git@github.com:kanduvisla/ckeditor.git extensions/ckeditor --recursive
 #
 ###
 
 
 ### ESSENTIALS EXTENSIONS
 #
-###git submodule add git://github.com/Solutions-Nitriques/static_section.git extensions/static_section --recursive
+###git submodule add git@github.com:Solutions-Nitriques/static_section.git extensions/static_section --recursive
 ###Seems to be buggy, alternative below:
-git submodule add git://github.com/vlad-ghita/limit_section_entries.git extensions/limit_section_entries --recursive
+git submodule add git@github.com:vlad-ghita/limit_section_entries.git extensions/limit_section_entries --recursive
 
  
-git submodule add git://github.com/ahwayakchih/improvedpageresolve.git extensions/improvedpageresolve --recursive
-git submodule add git://github.com/symphonists/globalresourceloader.git extensions/globalresourceloader --recursive
-git submodule add git://github.com/bauhouse/configuration.git extensions/configuration --recursive
-git submodule add git://github.com/symphonycms/content_type_mappings.git extensions/content_type_mappings --recursive
+git submodule add git@github.com:ahwayakchih/improvedpageresolve.git extensions/improvedpageresolve --recursive
+git submodule add git@github.com:symphonists/globalresourceloader.git extensions/globalresourceloader --recursive
+git submodule add git@github.com:bauhouse/configuration.git extensions/configuration --recursive
+git submodule add git@github.com:symphonycms/content_type_mappings.git extensions/content_type_mappings --recursive
 #
 ###
