8f87e569181f682db32a240e305ed4d35abe9733
David Anderson <dave@veodesign.co.uk>
2012-07-21 15:17:56 +0100

    Further installer tweaks

--- git_init_unix.sh
+++ git_init_unix.sh
@@ -6,37 +6,40 @@
 #
 ###
 
-#INITIAL - UNCOMMENT TO RUN AS A COMPLETE INSTALLER
+# INITIAL - UNCOMMENT TO RUN AS A COMPLETE INSTALLER
 
-git init
-git remote add workspace git@github.com:davjand/sym-workspace.git
-git fetch workspace
-git merge workspace/master --no-edit
+#git init
+#git remote add workspace git@github.com:davjand/sym-workspace.git
+#git fetch workspace
+#git merge workspace/master --no-edit
 
-#####
+###
 
 
-
-#Pull Symphony
+# Pull Symphony
 git remote add symphony git@github.com:symphonycms/symphony-2.git
 git fetch workspace
 git merge workspace/master --no-edit
 
+###
 
-#Sort out the git ignore from symphony
+# Sort out the git ignore from symphony
 rm .gitignore
 mv ./workspace.gitignore ./.gitignore
 
+###
 
-#grab extensions
+#Grab extensions
 #sh git_submodules.sh
 
+###
 
-#Init submodules
+# Init submodules
 git submodule update --init
 
+###
 
-#REMOTE REPO SETUP
+# REMOTE REPO SETUP
 git remote add origin git@github.com:davjand/veo-website.git
 
 git add -A
