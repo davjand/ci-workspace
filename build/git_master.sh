253b210674a9fc4c15730be0dac12c541e859889
David Anderson <dave@veodesign.co.uk>
2012-07-22 14:07:34 +0100

    Added master init script

---
+++ build/git_master.sh
@@ -0,0 +1,12 @@
+### GIT MASTER INIT SCRIPT
+#
+# This script can be copied into the root of an empty repo and used to set everything up
+# @author: David Anderson
+# @date: 20-07-2012
+#
+###
+
+git init
+git remote add workspace git@github.com:davjand/sym-workspace
+git pull workspace
+sh build/git_init.sh
