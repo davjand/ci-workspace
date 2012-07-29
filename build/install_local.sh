85cc2597396a51a13035ad0c50e98ee78506cfc0
David Anderson <dave@veodesign.co.uk>
2012-07-29 20:41:50 +0100

    Build Scripts Tweaked

--- build/install_local.sh
+++ build/install_local.sh
@@ -1,11 +1,11 @@
-### INSTALL.SH
+### INSTALL_LOCAL.SH
 #
 # Used to install the repository after a pull to a NEW local server
 #
 # INSTALL STEPS
 #
 # 1) Pull repository to server
-# 2) Run build/install.sh
+# 2) Run build/install_local.sh
 # 3) Visit /install on the server to complete the process
 # 
 # @author: David Anderson
---
+++ build/install_staging.sh
@@ -0,0 +1,31 @@
+### INSTALL_STATING.SH
+#
+# Used to install the repository onto a staging server
+#
+# INSTALL STEPS
+#
+# 1) Pull repository to server
+# 2) Run build/install_staging.sh
+# 3) Visit /install on the server to complete the process
+#
+# You will probable need to setup ssh keys for github as well
+# 
+# @author: David Anderson
+# @date: 20-07-2012
+#
+###
+
+###MAY NEED CHANGING
+
+git submodule update --init
+git submodule foreach git pull origin master
+
+#copy the install directory
+cp -r build/install/ public_html/install/
+
+
+#permission setup - needed for install, symphony 'should' sort out after
+chmod -R 755 public_html/workspace/
+chmod -R 755 public_html/manifest/
+chmod -R 755 public_html/install/
+chmod  755 public_html/
